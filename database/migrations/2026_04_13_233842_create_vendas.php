91e3a681913819113b25a4c1e625dab32bce84c6
Pedro Chassot <pedro.lcf07@aluno.ifsc.edu.br>
2026-04-13 21:00:12 -0300

    Criação da migration de Vendas

---
+++ database/migrations/2026_04_13_233842_create_vendas.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('funcionario_id');
+            $table->float('total_venda');
+            $table->foreignId('cliente_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendas');
+    }
+};
